fde671e7d1f71d8ccfc949515d901fe909168d98
Aleksandr Rokashevich <rokashevich@gmail.com>
2017-07-18 12:02:30 +0300

    Реализация --update
    Сохранение содержимого тэгов репозиторияев в var/xpt/session/tags/имя_тэга.

--- xpt.py
+++ xpt.py
@@ -1,9 +1,13 @@
 import argparse
 import os
+import urllib.request
 import re
+import shutil
 import sys
 
 sources_txt = os.path.join('etc', 'xpt', 'sources.txt')
+session_dir = os.path.join('var', 'xpt', 'session')
+session_tags_dir = os.path.join(session_dir, 'tags')
 
 
 class Session:
@@ -11,9 +15,20 @@ class Session:
         pass
 
     def update(self):
+        if os.path.exists(session_dir):
+            shutil.rmtree(session_dir)
+        os.makedirs(session_tags_dir)
         for line in open(sources_txt).read().splitlines():
             if re.match('^repo ', line):
-                print(line)
+                chunks = line.split()
+                url = chunks[1]
+                tags = chunks[2:] if len(chunks) > 2 else ['']
+                for tag in tags:
+                    packages_txt_url = url + '/' + tag + '/packages.txt'
+                    print('get ' + packages_txt_url)
+                    with open(os.path.join(session_tags_dir, tag if tag else '_no_tag_'), 'a') as f:
+                        for package in urllib.request.urlopen(packages_txt_url).read().decode().splitlines():
+                            f.write(url + '/' + package + '\n')
         return 0
 
     def install(self, nargs):
