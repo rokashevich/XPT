13cfc0711a06282032088c9b8494e914acdbfe46
Aleksandr Rokashevich <rokashevich@gmail.com>
2018-01-29 16:07:30 +0300

    -

--- src/xpt.go
+++ src/xpt.go
@@ -6,10 +6,19 @@ import "runtime"
 
 func main() {
 	fmt.Println("xpt ver. 0.0.0 (" + runtime.Version() + ")")
-	args := os.Args[1:]
-	if len(args) < 2 {usage();os.Exit(1)}
-	if args[0] != "install" {usage();os.Exit(1)}
-	args = args[1:] // Оставляем только package1 @ tag1 package2 package3 @ tag3 package4
+
+	command := os.Args[1]
+	if command != "install" {
+		usage()
+		os.Exit(1)
+	}
+
+	args := os.Args[2:] // package1 @ tag1 package2 package3 @ tag3 package4
+	if len(args) < 1 {
+		usage()
+		os.Exit(1)
+	}
+
 	for _, element := range args {
 		fmt.Println(element)
 	}
@@ -17,8 +26,8 @@ func main() {
 
 type xptPackage struct {
 	name string
-	tag string
-	url string
+	tag  string
+	url  string
 	size string
 }
 
